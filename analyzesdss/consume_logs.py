189c765de7d3e540fb0a28c7b7ac8ff13af2df4c
Dominik Moritz <domoritz@gmail.com>
2013-11-05 12:24:48 -0800

    Read logs into database

--- analyzesdss/consume_logs.py
+++ analyzesdss/consume_logs.py
@@ -1,13 +1,36 @@
+import csv
 import dataset
+
+TYPES = {}
 
 
 def consume(database, files):
     print database, files
-    db = dataset.connect(database)
 
-    table = db['sometable']
-    table.insert(dict(name='John Doe', age=37))
-    table.insert(dict(name='Jane Doe', age=34, gender='female'))
+    for logfile in files:
+        db = dataset.connect(database)
 
-    john = table.find_one(name='John Doe')
-    print john
+        table = db['logs']
+
+        rows = []
+        with open(logfile, 'rb') as f:
+            reader = csv.reader(f)
+
+            # ignore header
+            reader.next()
+
+            for row in reader:
+                data = {
+                    'client': row[9],
+                    'server': row[11],
+                    'db': row[12],
+                    'access': row[13],
+                    'elapsed': float(row[14]),
+                    'rows': int(row[16]),
+                    'query': row[17],
+                    'error': bool(int(row[18])),
+                    'error_msg': row[19]
+                }
+                rows.append(data)
+
+        table.insert_many(rows, types=TYPES)
