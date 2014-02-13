1aedbb3d75f0f1b776ee4d1d7257cfca6b8de18e
Dominik Moritz <domoritz@gmail.com>
2014-02-13 14:35:56 -0800

    Fix import for multiple files

--- workloadanalysis/main.py
+++ workloadanalysis/main.py
@@ -1,17 +1,19 @@
 """Query workload analysis.
 
 Usage:
-  qwla (sdss|sqlshare) consume [DATABASE] -i INPUT ...
-  qwla (sdss|sqlshare) explain CONFIG [DATABASE]
-  qwla (sdss|sqlshare) analyze [DATABASE] [--plots]
+  qwla (sdss|sqlshare) consume INPUT... [-d DATABASE]
+  qwla (sdss|sqlshare) explain CONFIG [-d DATABASE]
+  qwla (sdss|sqlshare) analyze [--plots] [-d DATABASE]
   qwla (-h | --help)
   qwla --version
 
 Options:
-  -i INPUT     Logs to be read into database
-  --plots      Show plots
-  -h --help    Show this screen.
-  --version    Show version.
+  INPUT...       Log files to be read into database
+  -d [DATABASE]  The database to read from or write into
+  CONFIG         How to connect to SQLServer
+  --plots        Show plots
+  -h --help      Show this screen.
+  --version      Show version.
 
 "For a moment, nothing happened. Then, after a second or so,
 nothing continued to happen."
@@ -25,10 +27,11 @@ import query_analysis
 def main():
     arguments = docopt(__doc__, version='SDSS Tools 0.0.1')
 
-    db = arguments['DATABASE'] or 'sqlite:///test.sqlite'
+    db = (('DATABASE' in arguments and arguments['DATABASE'])
+          or 'sqlite:///test.sqlite')
 
     if arguments['consume']:
-        consume_logs.consume(db, arguments['-i'], arguments['sdss'])
+        consume_logs.consume(db, arguments['INPUT'], arguments['sdss'])
 
     if arguments['explain']:
         config = {}
