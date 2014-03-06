b9657f0b82825171c577832f874e27cbf3f48794
Dominik Moritz <domoritz@gmail.com>
2014-03-06 14:17:29 -0800

    Summary of how many unique

--- workloadanalysis/summary.py
+++ workloadanalysis/summary.py
@@ -4,9 +4,10 @@ from utils import format_tabulate as ft
 
 
 def summarize_sdss(db):
-    datareleases = db.query('SELECT db, count(*) c, count(distinct query) dc FROM logs GROUP BY db ORDER BY c DESC')
+    datareleases = db.query('SELECT db, count(*) c, count(distinct query) dc, count(distinct query)*1.0/count(*) FROM logs GROUP BY db ORDER BY c DESC')
     print "Number of queries per data release:"
-    print tabulate(ft(datareleases), headers=['release', 'count', 'distinct'])
+    print tabulate(ft(datareleases),
+                   headers=['release', 'count', 'distinct', '% distinct'])
 
 
 def summarize(database, sdss):
