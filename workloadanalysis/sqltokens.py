ec96d6d91f3d81025e4c104b3f84bfa8b7e9eadb
Shrainik Jain <shrainik@gmail.com>
2014-03-07 00:06:13 -0800

    Get operators without executing the query

---
+++ workloadanalysis/sqltokens.py
@@ -0,0 +1,21 @@
+import sqlparse
+
+def get_tokens(q):
+    q = q.replace('[','"').replace(']','"').lower().replace('union all',';').strip()
+    if q[0] == '(':
+        q = q.strip('(').strip(')')
+    tokens = []
+    parsed = sqlparse.parse(q)
+    for item in parsed:
+        for t in item.tokens:
+            if t.value == ',' or t.value == ';':
+                continue
+            if str(t.ttype) == "None":
+                if str(t)[0] == '(':
+                    for token in get_tokens(str(t)):
+                        tokens.append(token)
+            elif str(t.ttype) == "Token.Text.Whitespace" or str(t.ttype) == 'Token.Wildcard':
+                continue
+            else:
+                tokens.append(str(t))
+    return tokens
