da9857f508df346b96fcd894b33c592bcc1b5b1b
Dominik Moritz <domoritz@gmail.com>
2014-03-28 13:39:14 -0700

    Move download scripts, make them work with from..to arguments

--- download/downloader.py
+++ download/downloader.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
 
 from datetime import date, timedelta
-import glob
-import re
-import sys
 import os
+
 
 def doMain():
     from optparse import OptionParser
@@ -12,36 +10,35 @@ def doMain():
     parser = OptionParser(usage = 'usage: %prog [options]')
     parser.add_option('-w','--web',dest='query', action='store_const', const='web',help='web log')
     parser.add_option('-s','--sql',dest='query', action='store_const', const='sql',help='SQL log')
+    parser.add_option('-b','--begin',dest='begin', help='when to begin as yyyy-mm-dd')
+    parser.add_option('-e','--end',dest='end', help='when to stop as yyyy-mm-dd')
 
-    (options, args) = parser.parse_args()
+    options, args = parser.parse_args()
 
     if options.query != 'web' and options.query != 'sql':
         parser.error('query type is not given')
 
-    # get the last one
-    files = glob.glob('sdsslog_%s_*.log' % options.query)
-    files.sort()
-
-    m = re.search('sdsslog_%s_(?P<year>\d{4})\.(?P<month>\d{2})\.(?P<day>\d{2})\.log' % options.query,files[-1])
-    if m is None:
-        print >> sys.stderr, "No existing uch file?"
-        sys.exit(-1)
-
-    y = int(m.group('year'))
-    mon = int(m.group('month'))
-    d = int(m.group('day'))
+    dateparts = options.begin.split('-')
+    y = int(dateparts[0])
+    mon = int(dateparts[1])
+    d = int(dateparts[2])
 
     delta = timedelta(days=1)
-    i = date(y,mon,d) + delta
-    end = date(2010,1,1)
+    i = date(y, mon, d)
+
+    dateparts = options.end.split('-')
+    y = int(dateparts[0])
+    mon = int(dateparts[1])
+    d = int(dateparts[2])
+    end = date(y, mon, d)
 
     queryType = '-w'
     if options.query == 'sql':
         queryType = '-s'
 
     while i < end:
-        ret = os.system("python download.py %s -y %d -m %d -d %d" % ( queryType, i.year, i.month, i.day ) )
-        print "%s: %04x" % ( i, ret )
+        ret = os.system("python download.py %s -y %d -m %d -d %d" % (queryType, i.year, i.month, i.day))
+        print "%s: %04x" % (i, ret)
         i += delta
 
 if __name__ == '__main__':
