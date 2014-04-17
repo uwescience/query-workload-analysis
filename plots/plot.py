c91d0af49a44e2b1c8e22974ac9aae0fa2fc20c5
Dominik Moritz <domoritz@gmail.com>
2014-04-17 12:14:44 -0700

    Refactor folders, fix plots

--- plots/plot.py
+++ plots/plot.py
@@ -5,26 +5,49 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 from prettyplotlib import brewer2mpl
 from prettyplotlib.colors import set1 as cs
+from matplotlib.ticker import FuncFormatter
 
 
-def read_csv(headers):
-    with open('../results/' + '_'.join(headers) + '.csv') as f:
+def to_percent(y, position):
+    # Ignore the passed in position. This has the effect of scaling the default
+    # tick locations.
+    return str(int(100 * y))
+
+    # The percent symbol needs escaping in latex
+    if mpl.rcParams['text.usetex'] == True:
+        return s + r'$\%$'
+    else:
+        return s + '%'
+
+
+def read_csv(headers, sdss):
+    folder = 'sdss' if sdss else 'sqlshare'
+    with open('../results/' + folder + '/' + '_'.join(headers) + '.csv') as f:
         return np.recfromcsv(f)
 
 
 def query_length():
     fig, ax = plt.subplots(1)
 
-    data = read_csv(['lengths', 'counts'])
-    ppl.plot(ax, data['lengths'], np.cumsum(data['counts']), label="Length", color=cs[0], linewidth=2, linestyle='-.')
+    data = read_csv(['lengths', 'counts'], True)
+    c = data['counts'].astype(float)
+    c /= sum(c)
+    ppl.plot(ax, data['lengths'], np.cumsum(c), label="Length", color=cs[0], linewidth=2, linestyle='-.')
 
-    data = read_csv(['compressed_lengths', 'counts'])
-    ppl.plot(ax, data['compressed_lengths'], np.cumsum(data['counts']), label="Compressed length", color=cs[1], linewidth=2, linestyle='--')
+    data = read_csv(['compressed_lengths', 'counts'], True)
+    c = data['counts'].astype(float)
+    c /= sum(c)
+    ppl.plot(ax, data['compressed_lengths'], np.cumsum(c), label="Compressed length", color=cs[1], linewidth=2, linestyle='--')
 
     ppl.legend(ax, loc='lower right')
 
+    formatter = FuncFormatter(to_percent)
+    plt.gca().yaxis.set_major_formatter(formatter)
+
     plt.xlabel('Query length in characters')
-    plt.ylabel('# of queries')
+    plt.ylabel('% of queries')
+
+    plt.ylim(0, 1)
 
     plt.show()
 
@@ -34,7 +57,7 @@ def query_length():
 def table_touch():
     fig, ax = plt.subplots(1)
 
-    data = read_csv(['touch', 'counts'])
+    data = read_csv(['touch', 'counts'], True)
     ppl.bar(ax, range(len(data['touch'])), data['counts'], xticklabels=data['touch'], grid='y', log=True)
 
     plt.xlabel('Table touch')
@@ -44,28 +67,41 @@ def table_touch():
 
     fig.savefig('plot_touch_sdss.pdf', format='pdf', transparent=True)
 
-def table_touch_sqlshare():
+def table_touch_cda():
     fig, ax = plt.subplots(1)
 
-    data = read_csv(['touch'])
-    print data
-    ppl.bar(ax, range(len(data['touch'])), data['count'], xticklabels=data['touch'], grid='y', log=True)
+    data = read_csv(['touch'], False)
+    c = data['count'].astype(float)
+    c /= sum(c)
+    ppl.plot(ax, data['touch'], np.cumsum(c), label="Touch sqlshare", color=cs[0], linewidth=2, linestyle='-.')
+
+    data = read_csv(['touch', 'counts'], True)
+    c = data['counts'].astype(float)
+    c /= sum(c)
+    ppl.plot(ax, data['touch'], np.cumsum(c), label="Touch sdss", color=cs[1], linewidth=2, linestyle='--')
+
+    ppl.legend(ax, loc='lower right')
+
+    formatter = FuncFormatter(to_percent)
+    plt.gca().yaxis.set_major_formatter(formatter)
 
     plt.xlabel('Table touch')
-    plt.ylabel('# of queries')
+    plt.ylabel('% of queries')
+
+    plt.ylim(0, 1)
 
     plt.show()
 
-    fig.savefig('plot_touch_sdss.pdf', format='pdf', transparent=True)
+    fig.savefig('plot_touch_cda.pdf', format='pdf', transparent=True)
 
 def logical_ops():
     fig, ax = plt.subplots(1)
 
-    data = read_csv(['logical_op', 'count'])
+    data = read_csv(['logical_op', 'count'], True)
     ppl.barh(range(len(data['logical_op'])), data['count'])
 
-    plt.xlabel('Logical operators')
-    plt.ylabel('# of queries')
+    plt.ylabel('Logical operators')
+    plt.xlabel('# of queries')
 
     plt.show()
 
@@ -76,5 +112,5 @@ if __name__ == '__main__':
 
     #query_length()
     #table_touch()
-    logical_ops()
-    #table_touch_sqlshare()
+    #logical_ops()
+    #table_touch_cda()
