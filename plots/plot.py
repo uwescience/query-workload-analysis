54db04f920fd2375623da848991e4ff84427b71d
Dominik Moritz <domoritz@gmail.com>
2014-04-22 15:25:04 -0700

    Nice plots, hurray

--- plots/plot.py
+++ plots/plot.py
@@ -20,6 +20,9 @@ def to_percent(y, position):
         return s + '%'
 
 
+formatter = FuncFormatter(to_percent)
+
+
 def read_csv(headers, sdss):
     folder = 'sdss' if sdss else 'sqlshare'
     with open('../results/' + folder + '/' + '_'.join(headers) + '.csv') as f:
@@ -32,22 +35,23 @@ def query_length():
     data = read_csv(['lengths', 'counts'], True)
     c = data['counts'].astype(float)
     c /= sum(c)
-    ppl.plot(ax, data['lengths'], np.cumsum(c), label="Length", color=cs[0], linewidth=2, linestyle='-.')
+    ppl.plot(ax, data['lengths'], np.cumsum(c), label="SDSS", color=cs[0], linewidth=2, ls='-.')
 
-    data = read_csv(['compressed_lengths', 'counts'], True)
-    c = data['counts'].astype(float)
+    data = read_csv(['lengths'], False)
+    data.sort(order='length')
+    c = data['count'].astype(float)
     c /= sum(c)
-    ppl.plot(ax, data['compressed_lengths'], np.cumsum(c), label="Compressed length", color=cs[1], linewidth=2, linestyle='--')
+    ppl.plot(ax, data['length'], np.cumsum(c), label="SQLShare", color=cs[1], linewidth=2, ls='--')
 
     ppl.legend(ax, loc='lower right')
 
-    formatter = FuncFormatter(to_percent)
     plt.gca().yaxis.set_major_formatter(formatter)
 
     plt.xlabel('Query length in characters')
     plt.ylabel('% of queries')
 
     plt.ylim(0, 1)
+    plt.xlim(0, 2500)
 
     plt.show()
 
@@ -68,27 +72,33 @@ def table_touch():
     fig.savefig('plot_touch_sdss.pdf', format='pdf', transparent=True)
 
 def table_touch_cda():
-    fig, ax = plt.subplots(1)
+    fig, [ax1, ax2] = plt.subplots(1, 2, sharey=True)
 
     data = read_csv(['touch'], False)
     data.sort(order='touch')
     c = data['count'].astype(float)
     c /= sum(c)
-    ppl.plot(ax, data['touch'], np.cumsum(c), label="Touch sqlshare", color=cs[0], linewidth=2, linestyle='-.')
+    ppl.plot(ax1, data['touch'], np.cumsum(c), label="SQLShare", color=cs[0], linewidth=2, linestyle='-.')
 
-    #ax = ax.twinx()
     data = read_csv(['touch', 'counts'], True)
     c = data['counts'].astype(float)
     c /= sum(c)
-    ppl.plot(ax, data['touch'], np.cumsum(c), label="Touch sdss", color=cs[1], linewidth=2, linestyle='--')
+    ppl.plot(ax2, data['touch'], np.cumsum(c), label="SDSS", color=cs[1], linewidth=2, linestyle='--')
 
-    ppl.legend(ax, loc='lower right')
+    ppl.legend(ax1, loc='lower right')
+    ppl.legend(ax2, loc='lower right')
 
-    formatter = FuncFormatter(to_percent)
-    plt.gca().yaxis.set_major_formatter(formatter)
+    ax1.yaxis.set_major_formatter(formatter)
+    ax2.yaxis.set_major_formatter(formatter)
 
-    plt.xlabel('Table touch')
-    plt.ylabel('% of queries')
+    ax1.set_xlim([0, 500])
+    ax1.set_xlim([0, 25])
+
+    #ax1.set_xlabel('Table touch')
+    fig.text(0.5, 0.02, "Table touch", ha='center')
+    ax1.set_ylabel('% of queries')
+
+    fig.subplots_adjust(wspace=0.1)
 
     plt.ylim(0, 1)
 
@@ -97,7 +107,7 @@ def table_touch_cda():
     fig.savefig('plot_touch_cda.pdf', format='pdf', transparent=True)
 
 def physical_ops():
-    fig, ax = plt.subplots(1)
+    fig, ax = plt.subplots(1, figsize=(8,4))
 
     data = read_csv(['physical_op', 'count'], True)
     data.sort(order='count')
@@ -110,12 +120,13 @@ def physical_ops():
     ypos = np.arange(len(data['physical_op']))
     ppl.barh(ax, ypos, c, yticklabels=data['physical_op'], grid='x', annotate=True)
 
-    plt.ylabel('Physical operator')
-    plt.xlabel('% of queries')
+    #ax.set_ylabel('Physical operator')
+    ax.set_xlabel('% of queries')
 
     #plt.subplots_adjust(bottom=.2, left=.3, right=.99, top=.9, hspace=.35)
 
-    plt.tight_layout(rect=[0, 0, 1, 0.6])
+    fig.tight_layout(rect=[0.03, 0, 1, 1])
+    fig.text(0.02, 0.55, 'Physical operator', rotation=90, va='center')
 
     plt.show()
 
