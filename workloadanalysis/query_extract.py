06cb09118605c1574381d1ae641e5c82e842281b
Dominik Moritz <domoritz@gmail.com>
2014-11-04 14:22:52 -0800

    extract columns

--- workloadanalysis/query_extract.py
+++ workloadanalysis/query_extract.py
@@ -9,6 +9,14 @@ visitor_logical_ops = lambda x: [x['operator']]
 visitor_physical_ops = lambda x: [x['physicalOp']]
 
 
+def visitor_columns(x):
+    cols = []
+    for table in x.get('columns').keys():
+        for col in x.get('columns')[table]:
+            cols.append(table + '.' + col)
+    return cols
+
+
 def visit_operators(query_plan, visitor):
     l = visitor(query_plan)
     for child in query_plan['children']:
@@ -17,28 +25,34 @@ def visit_operators(query_plan, visitor):
 
 
 def extract_tpch(db):
-    return extract(db, 'tpchqueries', 'tpch_tables', 'tpch_logops', 'tpch_physops')
+    return extract(db, 'tpchqueries', 'tpch_tables', 'tpch_columns', 'tpch_logops', 'tpch_physops')
 
 
 def extract_sdss(db):
-    return extract(db, 'explained', 'sdss_tables', 'sdss_logops', 'sdss_physops')
+    return extract(db, 'explained', 'sdss_tables', 'sdss_columns', 'sdss_logops', 'sdss_physops')
 
 
 def extract_sqlshare(db):
-    return extract(db, 'sqlshare_logs', 'sqlshare_tables', 'sqlshare_logops', 'sqlshare_physops')
+    return extract(db, 'sqlshare_logs', 'sqlshare_tables', 'sqlshare_columns', 'sqlshare_logops', 'sqlshare_physops')
 
 
-def extract(db, query_table, tables_name, logops_name, physops_name):
+def extract(db, query_table, tables_name, columns_name, logops_name, physops_name):
     datasetdb = dataset.connect(db)
 
     queries = datasetdb[query_table]
 
     tables = datasetdb[tables_name]
+    columns = datasetdb[columns_name]
     logops = datasetdb[logops_name]
     physops = datasetdb[physops_name]
 
     try:
         datasetdb.query("truncate table %s" % tables_name)
+    except sa.exc.ProgrammingError:
+        pass
+
+    try:
+        datasetdb.query("truncate table %s" % columns_name)
     except sa.exc.ProgrammingError:
         pass
 
@@ -70,5 +84,9 @@ def extract(db, query_table, tables_name, logops_name, physops_name):
         tables_ = visit_operators(plan, visitor_tables)
         tables.insert_many([
             {'query_id': query['id'], 'table': x} for x in tables_])
+
+        columns_ = visit_operators(plan, visitor_columns)
+        columns.insert_many([
+            {'query_id': query['id'], 'table': x} for x in columns_])
 
     datasetdb.commit()
