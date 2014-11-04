dd2c1ed8d50c6264970f1a65147992c647e56763
Dominik Moritz <domoritz@gmail.com>
2014-11-03 17:33:42 -0800

    op plots

--- plots/new_plot.py
+++ plots/new_plot.py
@@ -50,6 +50,65 @@ def load_data(metric, wls=workloads):
     return data
 
 
+def num_ops():
+    d = load_data("num_physops")
+
+    sns.set_context("paper", font_scale=1.3)
+    sns.set_style("whitegrid")
+
+    for w in workloads:
+        c = d[w]['count'].astype(float)
+        c /= sum(c)
+        plt.plot(d[w]['number'], np.cumsum(c),
+                 label=labels[w], color=colors[w], linewidth=2, ls=lines[w])
+
+    axes = plt.gca()
+
+    axes.set_ylim(0, 1)
+    axes.set_xlim(0, 300)
+
+    axes.yaxis.set_major_formatter(formatter)
+
+    axes.set_xlabel('Number of physical operators')
+    axes.set_ylabel('% of queries')
+
+    plt.legend(loc=4)
+    plt.tight_layout()
+
+    plt.savefig('plot_num_physops_cdf.pdf', format='pdf', transparent=True)
+    plt.show()
+
+
+def num_dist_ops():
+    d = load_data("num_dist_physops")
+
+    sns.set_context("paper", font_scale=1.3)
+    sns.set_style("whitegrid")
+
+    for w in workloads:
+        c = d[w]['count'].astype(float)
+        c /= sum(c)
+        plt.plot(d[w]['number'], np.cumsum(c),
+                 label=labels[w], color=colors[w], linewidth=2, ls=lines[w])
+
+    axes = plt.gca()
+
+    axes.set_ylim(0, 1)
+    axes.set_xlim(0, 12)
+
+    axes.yaxis.set_major_formatter(formatter)
+
+    axes.set_xlabel('Number of distinct physical operators')
+    axes.set_ylabel('% of queries')
+
+    plt.legend(loc=4)
+    plt.tight_layout()
+
+    plt.savefig('plot_num_dist_physops_cdf.pdf', format='pdf', transparent=True)
+    plt.show()
+
+
+
 def query_length():
     d = load_data("query_length")
 
@@ -61,8 +120,6 @@ def query_length():
         c /= sum(c)
         plt.plot(d[w]['char_length'], np.cumsum(c),
                  label=labels[w], color=colors[w], linewidth=2, ls=lines[w])
-        # sns.kdeplot(d[w], cumulative=True, label=labels[w],
-        #             bw=1, color=colors[w])
 
     axes = plt.gca()
 
@@ -88,8 +145,6 @@ def table_touch():
     sns.set_style("whitegrid")
 
     for w in workloads:
-        # sns.kdeplot(d[w], cumulative=True, label=labels[w],
-        #             bw=1, ax=axes[a[w]], color=colors[w], gridsize=1000)
         c = d[w]['count'].astype(float)
         c /= sum(c)
         plt.plot(d[w]['number'], np.cumsum(c),
@@ -120,8 +175,6 @@ def runtime():
     sns.set_style("whitegrid")
 
     for w in ws:
-        # sns.kdeplot(d[w], cumulative=True, label=labels[w],
-        #             color=colors[w])
         c = d[w]['count'].astype(float)
         c /= sum(c)
         plt.plot(d[w]['runtime'], np.cumsum(c),
@@ -145,6 +198,8 @@ def runtime():
 
 
 if __name__ == '__main__':
+    # num_ops()
+    num_dist_ops()
     # query_length()
-    table_touch()
+    # table_touch()
     # runtime()
