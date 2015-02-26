449141e147759f8a311496fb92cd7c5added0aba
Shrainik Jain <shrainik@gmail.com>
2015-02-26 10:29:37 -0800

    changing back to all of sqlshare

--- workloadanalysis/main.py
+++ workloadanalysis/main.py
@@ -91,7 +91,7 @@ def main():
         elif arguments['tpch']:
             query_analysis.analyze_tpch(db)
         else:
-            query_analysis.analyze_sqlshare(db, False)
+            query_analysis.analyze_sqlshare(db)
 
     if arguments['calcsimilarity']:
         if arguments['sqlshare']:
