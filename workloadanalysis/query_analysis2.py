daf35404f1c913d5e3861564e53432ba4361b332
Shrainik Jain <shrainik@gmail.com>
2015-02-26 19:25:03 -0800

    spelling error in imports.

--- workloadanalysis/query_analysis2.py
+++ workloadanalysis/query_analysis2.py
@@ -20,7 +20,7 @@ import math
 import numpy as np
 import dataset
 import sqlalchemy as sa
-from dataime import datetime
+from datatime import datetime
 from collections import Counter
 
 def analyse2(database):
