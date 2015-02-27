b2a7bfea8c45f958b35500cde73b43cb630597ce
Shrainik Jain <shrainik@gmail.com>
2015-02-26 19:25:59 -0800

    fixing imports again.

--- workloadanalysis/query_analysis2.py
+++ workloadanalysis/query_analysis2.py
@@ -20,7 +20,7 @@ import math
 import numpy as np
 import dataset
 import sqlalchemy as sa
-from datatime import datetime
+from datetime import datetime
 from collections import Counter
 
 def analyse2(database):
